c8c3e4ca96abbe108f24a21d025ac9032565fbbf
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-19 18:12:54 -0300

    insert select and refactor script

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+let n = document.querySelector('input.add')
+let lista = document.querySelector('select.numerosDigitados')
+let item = document.createElement('option')
+let valores = []
+
+
+
 function adicionarNumeros() {
-  let add = document.querySelector('input.add')
-  let numerosDigitados = document.querySelector('div.numerosDigitados')
-  let resposta = Number(add.value)
 
-
-  if (add.value.length == 0 || resposta > 100) {
+  if (n.value <= 0 || n.value > 100) {
     alert("INVÁLIDO! Digite um número entre 1 e 100")
 
+
+
+  } else if(n.value === item.value) {
+    alert("Número repetido, insira outro valor")
+
+
   } else {
-    numerosDigitados.innerHTML += ` Valor ${resposta} adicionado `
+    let resposta = Number(n.value)
+    let item = document.createElement('option')
+    lista.append(item)
+
+    item.innerHTML = ` Valor ${resposta} adicionado `
   }
-  console.log(add, numerosDigitados)
-
 }
-
+  console.log(lista)
 
 
 //numerosDigitados.append((` Valor ${resposta} adicionado `))
+/*
+else {
+  valores.push(Number(add.value))
+  let item = document.createElement('option')
+  item.text = ` Valor ${resposta} adicionado `
+  lista.appendChild(item)
+  */
